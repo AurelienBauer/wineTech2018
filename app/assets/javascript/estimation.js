219f4a7f512f70af50a3addb255b27677fbb9101
AurelienBauer <aurelien.bauer@epitech.eu>
2018-02-18 12:29:15 +0100

    One simule button

--- app/assets/javascript/estimation.js
+++ app/assets/javascript/estimation.js
@@ -4,6 +4,12 @@ var info = {
     "COLI" : 1.62
 };
 
+var export_percent = {
+    "dhl" : 1,
+    "fedex" : 0.90,
+    "coli" : 1.5
+};
+
 var transport = {
     "France" : 3.5 * 0.75,
     "Belgique" : 5 * 0.75,
@@ -19,32 +25,20 @@ var frais = {
     "Bresil" : 0
 };
 
-$('#valeur').change(function () {
-    calc_values();
-});
-
-$('#pays').change(function () {
-    calc_values();
-});
-
-$('#nbr_colis').change(function () {
-    calc_values();
-});
-
-$('#nbr_bouteilles').change(function () {
+$('#simulated').click(function () {
     calc_values();
 });
 
 $(document).ready(function() {
     $('select').material_select();
 });
-
-function get_html_cost(cost, country) {
+// 200 = 350
+function get_html_cost(cost, fee, country, imprt) {
     var html;
     if (country == "Bresil")
-        html = cost + " €</br>Taxe total: 80€";
+        html = cost + " €</br>Taxe total d'imporation: "+imprt+" €";
     else
-        html = cost + " €</br>Frais d'exportation : " + frais[country] + " €";
+        html = cost + " €</br>Frais d'exportation : " + fee + " €";
     return html;
 }
 
@@ -56,7 +50,6 @@ function calc_values() {
 
     if (!(["France", "Allemagne", "Belgique", "Bresil"].indexOf(country) > -1))
         country = "France";
-    console.dir(country);
     if (!val)
         val = 0;
     if (!colis)
@@ -74,9 +67,9 @@ function calc_values() {
     };
 
     var obj = {
-        'coli' : coli + frais[country],
-        'fedex':  fedex + frais[country],
-        'dhl' : dhl + frais[country]
+        'coli' : coli + frais[country] * export_percent['coli'],
+        'fedex':  fedex + frais[country] * export_percent['fedex'],
+        'dhl' : dhl + frais[country] * export_percent['dhl']
     };
     var result = [];
 
@@ -92,6 +85,7 @@ function calc_values() {
         var elem = $('#'+v[0]);
         elem.remove();
         $('#select').before(elem);
-        $('#'+v[0]+"_price").html(get_html_cost(prix[v[0]].toFixed(2), country));
+        $('#'+v[0]+"_price").html(get_html_cost(prix[v[0]].toFixed(2),
+            frais[country] * export_percent[v[0]], country, (3/2 * val).toFixed(2)));
     });
 }
